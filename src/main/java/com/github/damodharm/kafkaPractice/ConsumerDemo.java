4823740adfb505329b7ad3bb1da73a82abcb90cc
Damodhar Reddy Munagala <damodhar.munagala@thoughtworks.com>
2020-01-13 21:46:18 +0530

    Added Dockerfile and made changes to the existing code.

--- src/main/java/com/github/damodharm/kafkaPractice/ConsumerDemo.java
+++ src/main/java/com/github/damodharm/kafkaPractice/ConsumerDemo.java
@@ -7,7 +7,6 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.scope.DefaultScopedObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -29,20 +28,20 @@ public class ConsumerDemo {
     Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
 
     // create properties
+    @Bean
     public Properties properties() {
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "input-topic-group");
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         return properties;
     }
 
     // create consumer
     @Bean
     public KafkaConsumer<String, String> createConsumer() {
-        System.out.println("In createConsumer() method....................");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties());
         consumer.subscribe(Collections.singletonList(TOPIC_NAME));
         return  consumer;
@@ -51,10 +50,19 @@ public class ConsumerDemo {
     // subscribe to topic
     @PostConstruct
     public void subscribeToTopic() {
-        System.out.println("Subscribed to topic.............................");
-        ConsumerRecords<String, String> records = createConsumer().poll(Duration.ofMillis(100));
+        KafkaConsumer<String, String> consumer = createConsumer();
+        while(true) {
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
         for (ConsumerRecord<String, String> record: records) {
-            logger.info(String.format("This data is being consumed by consumers. \nTopic : %s\nOffset : %d\nPartition : %d", record.topic(), record.offset(), record.partition()));
+            logger.info(String.format("Topic : %s - Key : %s - Value : %s - Topic : %s - Partition : %s", record.topic(), record.key(), record.value(), record.topic(), record.partition()));
+        }
+        consumer.commitAsync();
         }
     }
+
+    /*
+    * As we are using consumer groups. If only one consumer is running then it will read from all the partitions.
+    * If other consumer comes up then rebalancing happens then one consumer will read from one partition and the other consumer will read from other partition.
+    * See the logs for clear understanding of rebalancing in kafkaConsumerGroups.
+    * */
 }
